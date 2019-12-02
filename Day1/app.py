b5e4901ea1fb90bdeea8c82792555f2c16e98109
Chris Hodges <chris.hodges@gmail.com>
2019-12-01 23:27:47 -0600

    Day 1, part 2

--- Day1/app.py
+++ Day1/app.py
@@ -1,4 +1,11 @@
 
+def get_required_fuel_for_module(module_mass: int) -> int:
+    fuel_for_current_mass = get_required_fuel(module_mass)
+    if fuel_for_current_mass < 0:
+        return 0
+    else:
+        return get_required_fuel_for_module(fuel_for_current_mass) + fuel_for_current_mass
+
 
 def get_required_fuel(mass: int) -> int:
     return mass // 3 - 2
@@ -6,3 +13,7 @@ def get_required_fuel(mass: int) -> int:
 
 def get_required_fuel_on_list(list_of_masses: list) -> int:
     return sum(map(get_required_fuel, list_of_masses))
+
+
+def get_final_required_fuel_on_list(list_of_masses: list) -> int:
+    return sum(map(get_required_fuel_for_module, list_of_masses))
--- Day1/test_day1.py
+++ Day1/test_day1.py
@@ -1,6 +1,13 @@
 from Day1 import app
 import pytest
-import os
+from typing import List
+
+
+@pytest.fixture()
+def input_data() -> List[int]:
+    with open('./input.txt', 'r') as inputFile:
+        mass_list = map(int, inputFile.readlines())
+    return mass_list
 
 
 @pytest.mark.parametrize("mass, expected_fuel", [
@@ -8,12 +15,21 @@ import os
     (14, 2),
     (1969, 654),
     (100756, 33583)])
-def test_example_fuel_requirements(mass: int, expected_fuel:int):
+def test_example_fuel_requirements(mass: int, expected_fuel: int):
     assert app.get_required_fuel(mass) == expected_fuel
 
 
-def test_input_from_file():
-    with open('./input.txt', 'r') as inputFile:
-        mass_list = map(int, inputFile.readlines())
-        assert app.get_required_fuel_on_list(mass_list) == 3456641
+@pytest.mark.parametrize("mass, expected_fuel", [
+    (14, 2),
+    (1969, 966),
+    (100756, 50346)])
+def test_example_final_fuel_requirements(mass: int, expected_fuel: int):
+    assert app.get_required_fuel_for_module(mass) == expected_fuel
 
+
+def test_part_1(input_data):
+    assert app.get_required_fuel_on_list(input_data) == 3456641
+
+
+def test_part_2(input_data):
+    assert app.get_final_required_fuel_on_list(input_data) == 5182078
