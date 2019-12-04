94cbff86b2a45b0f5a6b91c1be7b2fa983f4033d
Chris Hodges <chris.hodges@gmail.com>
2019-12-03 21:13:23 -0600

    Finish Day2

--- Scala/src/main/scala/Day2/Day2.scala
+++ Scala/src/main/scala/Day2/Day2.scala
@@ -13,11 +13,13 @@ object ProgramAlarm1202 {
   val memory: Vector[Int] = Vector.empty
   val initial_program_counter: Int = 0
   val instruction_size = 4
+  val question_input = Vector(1, 0, 0, 3, 1, 1, 2, 3, 1, 3, 4, 3, 1, 5, 0, 3, 2, 1, 10, 19, 2, 9, 19, 23, 1, 9, 23, 27, 2, 27, 9, 31, 1, 31, 5, 35, 2, 35, 9, 39, 1, 39, 10, 43, 2, 43, 13, 47, 1, 47, 6, 51, 2, 51, 10, 55, 1, 9, 55, 59, 2, 6, 59, 63, 1, 63, 6, 67, 1, 67, 10, 71, 1, 71, 10, 75, 2, 9, 75, 79, 1, 5, 79, 83, 2, 9, 83, 87, 1, 87, 9, 91, 2, 91, 13, 95, 1, 95, 9, 99, 1, 99, 6, 103, 2, 103, 6, 107, 1, 107, 5, 111, 1, 13, 111, 115, 2, 115, 6, 119, 1, 119, 5, 123, 1, 2, 123, 127, 1, 6, 127, 0, 99, 2, 14, 0, 0)
+
   def executeInstruction(op: OpCode, memory: Vector[Int]): Option[Vector[Int]] = {
     op match {
       case Terminate() => None
-      case Add(left, right, dest) => Some(memory.updated(dest, left+right))
-      case Multiply(left, right, dest) => Some(memory.updated(dest, left*right))
+      case Add(left, right, dest) => Some(memory.updated(dest, memory(left) + memory(right)))
+      case Multiply(left, right, dest) => Some(memory.updated(dest, memory(left) * memory(right)))
     }
   }
 
@@ -25,20 +27,30 @@ object ProgramAlarm1202 {
     val LEFT_POS = 1
     val RIGHT_POS = 2
     val DEST_POS = 3
-    if(instruction.contains(99)) {
-      Terminate()
-    }
     instruction.head match {
-      case 0 => Add(instruction(LEFT_POS), instruction(RIGHT_POS), instruction(DEST_POS))
-      case 1 => Multiply(instruction(LEFT_POS), instruction(RIGHT_POS), instruction(DEST_POS))
+      case 1 => Add(instruction(LEFT_POS), instruction(RIGHT_POS), instruction(DEST_POS))
+      case 2 => Multiply(instruction(LEFT_POS), instruction(RIGHT_POS), instruction(DEST_POS))
+      case 99 => Terminate()
     }
   }
 
   def step(memory: Vector[Int], programCounter: Int): Option[Vector[Int]] = {
-    executeInstruction(parseNextInstruction(memory.slice(programCounter, programCounter+instruction_size)), memory)
+    executeInstruction(parseNextInstruction(memory.slice(programCounter, programCounter + instruction_size)), memory)
   }
 
-  def runProgram(memory: Vector[Int], programCounter: Int): Vector[Int] = {
-    step(memory, initial_program_counter).fold(memory)(runProgram(memory, programCounter+4))
+  def runProgram(memory: Vector[Int], programCounter: Int = 0): Vector[Int] = {
+    step(memory, programCounter).fold(memory)(runProgram(_, programCounter + 4))
+  }
+
+  def findInputsToCreateMagicNumber(magicNumber: Int): Option[(Int, Int)] = {
+    // Brute force!
+    for (noun <- 0 to 99) {
+      for (verb <- 0 to 99) {
+        if(runProgram(question_input.updated(1, noun).updated(2, verb)).head == magicNumber)
+          return Some(noun, verb)
+      }
+    }
+    None
   }
 }
+
--- Scala/src/test/scala/Day2/Day2Test.scala
+++ Scala/src/test/scala/Day2/Day2Test.scala
@@ -1,11 +1,59 @@
 import org.scalatest.FunSuite
+import Day2.ProgramAlarm1202._
 
 class ProgramAlarm1202Test extends FunSuite {
   val additionProgram = Vector(1, 0, 0, 0, 99)
   val multiplicationProgram = Vector(2, 3, 0, 3, 99)
   val anotherMultiplicationProgram = Vector(2, 4, 4, 5, 99, 0)
+  val terminationProgram = Vector(99, 0, 0, 0)
+  val test_program_99_sq = Vector(2,4,4,5,99,0)
+  val test_program_medium = Vector(1,1,1,4,99,5,6,0,99)
+
 
   test("test addition parsing") {
-    assert(Day2.ProgramAlarm1202.parseNextInstruction(additionProgram.take(4)) == Day2.Add(0, 0, 0))
+    assert(parseNextInstruction(additionProgram.take(4)) == Day2.Add(0, 0, 0))
+  }
+  test("test multiplication parsing") {
+    assert(parseNextInstruction(multiplicationProgram.take(4)) == Day2.Multiply(3, 0 , 3))
+  }
+  test( "test termination parsing") {
+    assert(parseNextInstruction(terminationProgram) == Day2.Terminate())
+  }
+
+  test("test multiplication operation") {
+    assert(executeInstruction(Day2.Multiply(3, 0, 3), multiplicationProgram) == Some(Vector(2,3,0,6,99)))
+  }
+
+  test("test addition operation") {
+    assert(executeInstruction(Day2.Add(0, 0, 0), additionProgram) == Some(Vector(2,0,0,0,99)))
+  }
+
+  test( "run addition program step by step") {
+    assert(step(additionProgram, 0) == Some(Vector(2,0,0,0,99)))
+    assert(step(additionProgram, 4) == None)
+  }
+
+  test( "run addition program") {
+    assert(runProgram(additionProgram) == Vector(2,0,0,0,99))
+  }
+
+  test( "run multiplication program") {
+    assert(runProgram(multiplicationProgram) == Vector(2,3,0,6,99))
+  }
+  
+  test( "run 99 squared test") {
+    assert(runProgram(test_program_99_sq) == Vector(2,4,4,5,99,9801))
+  }
+
+  test( "run medium size test") {
+    assert(runProgram(test_program_medium) == Vector(30,1,1,4,2,5,6,0,99))
+  }
+
+  test( "test with puzzle input") {
+    assert(runProgram(question_input.updated(1,12).updated(2,2)).head == 9581917)
+  }
+
+  test("look for noun and verb that produce 19690720") {
+    assert(findInputsToCreateMagicNumber(19690720) == Some(25,5))
   }
 }
