7f3f8605284d24f64277e7a78120efb9fb2fe623
Chris Hodges <chris.hodges@adtran.com>
2019-12-04 12:14:15 -0600

    DAY 3 WIP

--- Python/Day3/crossed_wires.py
+++ Python/Day3/crossed_wires.py
@@ -19,22 +19,41 @@ class Vector(object):
         return "{}{}".format(self.direction.value, str(self.magnitude))
 
     def __eq__(self, other):
+        print("Hello")
         return self.direction == other.direction and self.magnitude == other.magnitude
 
     direction: Direction
     magnitude: int
 
 
+class Grid(object):
+    MAX_MAGNITUDE = 1000
+    ORIGIN = (MAX_MAGNITUDE, MAX_MAGNITUDE)
+    grid = []
+
+    def __init__(self):
+        self.grid = [[0 for x in range(0, self.MAX_MAGNITUDE*2+1)]
+                        for y in range(0, self.MAX_MAGNITUDE*2+1)]
+
+    def __repr__(self):
+        pass
+
+    def update_grid_with_vector_list(self, vlist: List[Vector], current_point: (int, int)):
+        for this_vector in vlist:
+
+
+
 class CrossedWires(object):
     def __init__(self, wire1: str, wire2: str):
-        self.vectors = [self.parseWire(wire1), self.parseWire(wire2)]
+        self.grid = Grid()
+        self.vectors = [self.parse_wire(wire1), self.parse_wire(wire2)]
 
-    def parseWire(self, wire: str) -> List[Vector]:
+    def parse_wire(self, wire: str) -> List[Vector]:
         vector_list = []
         for raw_vector in wire.split(','):
             vector = Vector()
             vector.direction = self.decode_direction[raw_vector[0]]
-            vector.magnitude = str(raw_vector[1:])
+            vector.magnitude = int(raw_vector[1:])
             vector_list.append(vector)
         return vector_list
 
